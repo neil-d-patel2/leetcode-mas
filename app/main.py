348ab906c01ce5cf2addb6b3ebe208beabd2f9ee
Neil <ebox381@gmail.com>
2026-06-27 21:48:47 -0400

    feature 1 done

--- app/main.py
+++ app/main.py
@@ -1,2 +1,20 @@
-def main(name: str):
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: str):
     print(f"Hello {name}")
+
+
+@app.command()
+def leetcoach(name: str, solve: bool = False):
+    if solve:
+        print(f"Welcome to mas leetcoach! Lets solve.")
+    else:
+        print(f"Hello, I hope you are enjoying leetcoach!")
+
+
+if __name__ == "__main__":
+    app()
